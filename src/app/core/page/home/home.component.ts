0078447e482fdd297577e800d28dbaa892df4852
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2021-04-14 22:13:16 +0200

    feat(home): added list of all pokemon with mat pagination

--- src/app/core/page/home/home.component.ts
+++ src/app/core/page/home/home.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from 'src/app/shared/service/pokemon/pokemon.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass'],
 })
+
 export class HomeComponent implements OnInit {
   pokemon: any;
   pokemons: any;
+  isLoading = false;
+  currentPage = 0;
+  pokemonsToDisplay = [];
+  pokemonsPerPage = 20;
 
   constructor(
     private pokeService: PokemonService
@@ -19,15 +25,12 @@ export class HomeComponent implements OnInit {
   }
 
   async initialize(): Promise<void> {
-    await this.get("143");
     await this.getAll();
   }
 
   async get(id: string): Promise<any> {
     try {
       this.pokemon = this.pokeService.find(id);
-
-      return;
     } catch (error) {
       throw new Error(error);
     }
@@ -35,12 +38,32 @@ export class HomeComponent implements OnInit {
 
   async getAll(): Promise<any> {
     try {
-      this.pokemons = this.pokeService.getAll();
+      this.pokemons = await this.pokeService.getAll();
+      this.pokemons = this.pokemons.results;
 
-      console.log(this.pokemons);
-      return;
+      for (let i = 0; i <= this.pokemonsPerPage - 1; i++) {
+        if (this.pokemons[i]) {
+          this.pokemonsToDisplay.push(this.pokemons[i]);
+        }
+      }
+
+      console.log(this.pokemonsToDisplay);
     } catch (error) {
       throw new Error(error);
     }
+  }
+  onChangedPage(pageData: PageEvent) {
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.pokemonsPerPage = pageData.pageSize;
+    this.pokemonsToDisplay = [];
+    const newIndex = this.pokemonsPerPage * pageData.pageIndex;
+    for (let i = newIndex; i <= newIndex + this.pokemonsPerPage - 1; i++) {
+      if (this.pokemons[i]) {
+        this.pokemonsToDisplay.push(this.pokemons[i]);
+      }
+    }
+    console.log(this.pokemonsToDisplay);
+    this.isLoading = false;
   }
 }
--- src/app/shared/service/pokemon/pokemon.service.ts
+++ src/app/shared/service/pokemon/pokemon.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { PokeApiService } from "../poke_api/poke_api.service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
+  currentPokemonsSubject = new BehaviorSubject(null);
+
   constructor(private pokeApiService: PokeApiService) { }
 
-  getAll(): Promise<any> {
-    return this.pokeApiService.get('/pokemon/');
+  getCurrentPokemon(): Observable<any> {
+    return this.currentPokemonsSubject.asObservable();
   }
 
-  find(id: string): Promise<any> {
+  getCurrentPokemonValue() {
+    return this.currentPokemonsSubject.getValue();
+  }
+
+  setPokemon(pokemon): void {
+    this.currentPokemonsSubject.next(pokemon);
+  }
+
+  getAll(offset: string = "0", limit: string = "1118") {
+    const pokemons = this.pokeApiService.get(`/pokemon/?offset=${offset}&limit=${limit}`);
+
+    this.setPokemon(pokemons);
+    return this.getCurrentPokemonValue();
+  }
+
+  find(id: string) {
     return this.pokeApiService.get('/pokemon/' + id);
   }
 }
